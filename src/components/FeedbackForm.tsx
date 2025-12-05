fb2f6287bc325a2b95c46904991d989d15c056aa
stryczynski <stryczynski15@gmail.com>
2025-12-05 21:44:50 +0100

    Refactor FeedbackForm to extract handleChange function for better readability

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -4,18 +4,19 @@ import { MAX_CHARACTERS } from "../lib/constants";
 export default function FeedbackForm() {
   const [text, setText] = useState("");
   const charactersCount = MAX_CHARACTERS - text.length;
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = event.target.value;
+    if (newText.length > MAX_CHARACTERS) {
+      return;
+    }
+    setText(newText);
+  };
 
   return (
     <form className="form">
       <textarea
         value={text}
-        onChange={(e) => {
-          const newText = e.target.value;
-          if (newText.length > MAX_CHARACTERS) {
-            return;
-          }
-          setText(newText);
-        }}
+        onChange={handleChange}
         id="feedback-textarea"
         placeholder=""
         spellCheck={false}
